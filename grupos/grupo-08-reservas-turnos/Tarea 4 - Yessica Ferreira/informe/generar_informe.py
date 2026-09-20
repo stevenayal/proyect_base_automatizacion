c0fa8b442f4139b91a1887e2ca507bdd9789aa2e
jessyferreira <jessyferreira78@gmail.com>
2026-09-19 23:22:39 -0300

    corregir generación de reporte PDF en CI

--- grupos/grupo-08-reservas-turnos/Tarea 4 - Yessica Ferreira/informe/generar_informe.py
+++ grupos/grupo-08-reservas-turnos/Tarea 4 - Yessica Ferreira/informe/generar_informe.py
@@ -8,7 +8,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 JTL = BASE_DIR / "resultados" / "prueba_rate_limit.jtl"
-PDF = BASE_DIR / "informe" / "Informe_Prueba_Rendimiento.pdf"
+PDF = Path("newman") / "Informe_Prueba_Rendimiento.pdf"
 
 
 df = pd.read_csv(JTL)
